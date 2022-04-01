daa42e19c658d5952601d1ecaa16716f3b9f0785
alejandro <alejandro@uct.geocuba.cu>
2022-04-01 11:48:23 -0400

    initial

---
+++ src/components/Header.vue
@@ -0,0 +1,203 @@
+<template>
+  <div>
+    <header class="header">
+      <p class="manage">Manage <b>Employees</b></p>
+      <div class="buttons">
+        <ul>
+          <li>
+            <button class="btnDel" :disabled="!checked.length > 0" @click="deleteUsers"><span class="ico icoDel">-</span><b>Delete</b></button>
+          </li>
+          <li>
+            <button class="btnAdd" @click="showModal = true"><span class="ico icoAdd">+</span><b>Add New Employee</b></button>
+          </li>
+        </ul>
+      </div>
+    </header>
+    <Table @disableBtn="disableBtn"/>
+
+    <ModalAdd :show="showModal" @close="showModal = false">
+      <template #header>
+        <h3>Add user</h3>
+      </template>
+      <template #body>
+        <div class="input-container">
+          <div class="row">
+            <div class="column">
+              <p>First Name: <input type="text" v-model="firstName" placeholder="First Name"></p>
+              <p>Last Name: <input type="text" v-model="lastName" placeholder="Last Name"></p>
+              <p>Email: <input type="email" v-model="email" placeholder="Email"></p>
+            </div>
+            <div class="column">
+              <p>Age: <input type="number" v-model="age" placeholder="Age"></p>
+              <p>Grade: <input type="text" v-model="grade" placeholder="Grade"></p>
+            </div>
+          </div>
+        </div>
+      </template>
+      <template #footer>
+        <div class="buttons">
+          <ul>
+            <li>
+              <button
+                  class="modal-default-button"
+                  @click="showModal = false"
+              >CANCEL</button>
+            </li>
+            <li>
+              <button
+                  class="modal-default-button"
+              >OK</button>
+            </li>
+          </ul>
+        </div>
+      </template>
+    </ModalAdd>
+
+  </div>
+</template>
+
+<script>
+import Table from "@/components/Table";
+import ModalAdd from "@/components/ModalAdd";
+export default {
+  name: `Header`,
+  components: {ModalAdd, Table},
+  data () {
+    return {
+      showModal: false,
+      disableDelete: false,
+      checked: [],
+      firstName: '',
+      lastName: '',
+      email: '',
+      age: '',
+      grade: ''
+    }
+  },
+  methods : {
+    disableBtn(v){
+      this.checked = v
+    },
+    deleteUsers () {
+      debugger
+    },
+    addUser () {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+.header {
+  background-color: #0a256a;
+  opacity: 80%;
+  height: 70px;
+  border-radius: 5px;
+  padding: 10px;
+}
+
+.manage {
+  color: white;
+  float: left;
+  font-size: 30px;
+  margin-left: 20px;
+  margin-top: 20px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+
+.buttons ul {
+  display: flex;
+  justify-content: flex-end;
+  margin: 0;
+  padding: 15px;
+  align-items: baseline;
+}
+.buttons ul li {
+  display: inline-block;
+  margin: 0 5px;
+  align-items: baseline;
+}
+.buttons ul li button {
+  cursor: pointer;
+}
+
+.btnDel:disabled,
+.btnDel[disabled]{
+  border: 1px solid #999999;
+  background-color: #cccccc;
+  color: #666666;
+}
+
+.btnAdd {
+  height: 40px;
+  width: 222px;
+  color: white;
+  background-color: green;
+  display:inline-block;
+  text-align:center;
+  text-decoration:none;
+  border: 1px solid transparent;
+  font-size: 0.9rem;
+  border-radius: 3px;
+}
+.btnDel {
+  height: 40px;
+  width: 117px;
+  color: white;
+  background-color: red;
+  display:inline-block;
+  text-align:center;
+  text-decoration:none;
+  border: 1px solid transparent;
+  font-size: 0.9rem;
+  border-radius: 3px;
+}
+
+.ico {
+  position: absolute;
+  width: 22px;
+  height: 22px;
+  font-size: 20px;
+  -moz-border-radius: 50%;
+  -webkit-border-radius: 50%;
+  border-radius: 50%;
+  background: white;
+  margin-left: -30px;
+  margin-top: -2px;
+}
+
+.icoDel {
+  color: red;
+}
+.icoAdd {
+  color: green;
+}
+
+.input-container  {
+  display: flex;
+  flex-direction: column;
+  background-color: beige;
+  border-radius: 3px;
+  padding: 1em;
+}
+.input-container input {
+  flex: 6;
+  padding: .5em;
+  margin-bottom: 1em;
+}
+
+.row {
+  display: flex;
+  justify-content: center;
+}
+.column {
+  display: flex;
+  flex-direction: column;
+  padding: 1em;
+}
+.modal-default-button{
+  padding: 5px;
+}
+
+</style>
---
+++ src/components/Table.vue
@@ -0,0 +1,208 @@
+<template>
+  <div class="table-container">
+    <table class="table">
+      <thead>
+      <tr>
+        <th><input type="checkbox"></th>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Email</th>
+        <th>Age</th>
+        <th>Grade</th>
+        <th>Actions</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="item in items" :key="item.id" >
+        <td><input type="checkbox" v-model="checked" :id="item.id" :value="item.id" @change="checkBtn()"></td>
+        <td>{{ item.firstName }}</td>
+        <td>{{ item.lastName }}</td>
+        <td>{{ item.email }}</td>
+        <td>{{ item.age }}</td>
+        <td>{{ item.grade }}</td>
+        <td>
+          <Edit style="color: #adad0e"/>
+          <Delete style="color: red"/>
+        </td>
+      </tr>
+      </tbody>
+    </table>
+    <div class="footer-tools">
+      <div class="list-items">
+        Showing {{ }} out of {{ }} entries
+      </div>
+      <div class="pages">
+        <ul>
+          <li><button>Previous</button></li>
+          <li><button class="active">1</button></li>
+          <li><button>2</button></li>
+          <li><button>3</button></li>
+          <li><button>4</button></li>
+          <li><button>Next</button></li>
+        </ul>
+      </div>
+    </div>
+    {{ checked }}
+  </div>
+
+</template>
+
+<script>
+import Edit from 'vue-material-design-icons/Pencil'
+import Delete from 'vue-material-design-icons/Delete'
+export default {
+  name: `Table`,
+  components: {
+    Edit,
+    Delete
+  },
+  data () {
+    return {
+      items: [
+        {
+          id: '1',
+          firstName: 'asasasas',
+          lastName: 'ssssssss ssssssssssss',
+          email: 'asadsdasd@gmail.com',
+          age: '23',
+          grade: '2nd'
+        },{
+        id: '2',
+          firstName: 'asasasas',
+          lastName: 'ssssssss ssssssssssss',
+          email: 'asadsdasd@gmail.com',
+          age: '23',
+          grade: '2nd'
+        },{
+        id: '3',
+          firstName: 'asasasas',
+          lastName: 'ssssssss ssssssssssss',
+          email: 'asadsdasd@gmail.com',
+          age: '23',
+          grade: '2nd'
+        },{
+        id: '4',
+          firstName: 'asasasas',
+          lastName: 'ssssssss ssssssssssss',
+          email: 'asadsdasd@gmail.com',
+          age: '23',
+          grade: '2nd'
+        },
+      ],
+      checked: []
+    }
+  },
+  methods: {
+    checkBtn(){
+      this.$emit('disableBtn', this.checked)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.table-container {
+  border-collapse: collapse;
+  width: 100%;
+  color: #6f6f84;
+  font-family: AnjaliOldLipi;
+}
+.table-container .footer-tools .pages button {
+  border-collapse: collapse;
+  width: 100%;
+  color: #6f6f84;
+  font-family: AnjaliOldLipi;
+  font-size: 1rem;
+}
+.table-container .table {
+  border-collapse: collapse;
+  width: 100%;
+  color: #6f6f84;
+  font-family: AnjaliOldLipi;
+}
+.table-container .table,
+.table-container .table th,
+.table-container .table td {
+  padding: 2px 2px;
+}
+
+.table-container .table th {
+  font-weight: bolder;
+  text-align: left;
+  height: 50px;
+  border-bottom: solid 2px #b6b6bb;
+}
+.table-container .table td {
+  text-align: left;
+  height: 50px;
+  border-bottom: solid 1px #b6b6bb;
+}
+
+.table-container .table tbody tr:hover {
+  background-color: #e1e1e7;
+}
+
+.btn-container ul {
+  display: flex;
+  justify-content: flex-start;
+  margin: 0;
+  align-items: baseline;
+}
+.btn-container ul li {
+  display: inline-block;
+  margin: 0 5px;
+  align-items: baseline;
+}
+.btn:hover {
+  cursor: pointer;
+}
+
+.table-container .footer-tools {
+  padding: 12px;
+  display: flex;
+  justify-content: left;
+  align-items: baseline;
+}
+.table-container .footer-tools .list-items {
+  width: 50%;
+}
+.table-container .footer-tools .pages {
+  margin-left: auto;
+  margin-right: 0;
+  width: 50%;
+}
+.table-container .footer-tools .pages ul{
+  padding: 0;
+  margin: 0;
+  display: flex;
+  align-items: baseline;
+  justify-content: flex-end;
+}
+.table-container .footer-tools .pages ul li{
+  display: inline-block;
+  margin: 0 2px;
+}
+.table-container .footer-tools .pages ul li button{
+  width: 100%;
+  box-sizing: border-box;
+  border: 0;
+  border-radius: 3px;
+  background: transparent;
+  cursor: pointer;
+}
+.table-container .footer-tools .pages ul li button:hover{
+  background: #2c2cfd;
+  color: white;
+}
+.table-container .footer-tools .pages ul li button.active{
+  background-color: #2c2cfd;
+  border-radius: 3px;
+  color: white;
+}
+.table-container .footer-tools .pages ul li button,
+.table-container .footer-tools .pages ul li span{
+  padding: 6px 12px;
+}
+
+
+</style>
